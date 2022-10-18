075d1a1826efc5b6e2e6cdedd8c61c181be31763
Eric Henry <correia.eh@gmail.com>
2022-10-18 14:05:20 -0400

    fix diagnostics shortcuts

--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -160,11 +160,11 @@ local mappings = {
     i = { "<cmd>LspInfo<cr>", "Info" },
     I = { "<cmd>LspInstallInfo<cr>", "Installer Info" },
     j = {
-      "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>",
+      '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>',
       "Next Diagnostic",
     },
     k = {
-      "<cmd>lua vim.lsp.diagnostic.goto_prev()<cr>",
+      '<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>',
       "Prev Diagnostic",
     },
     l = { "<cmd>lua vim.lsp.codelens.run()<cr>", "CodeLens Action" },
